725df54060a230238497fb0d967bc77c6e7d9916
CaptainLiao <15196004361@163.com>
2016-07-10 12:03:38 +0800

    Updata

--- script/main.js
+++ script/main.js
@@ -1,10 +1,20 @@
-/*$(function(){
-	var $social = $("#about .myself span");
-	console.log($social.length);
-	$social.on('mouseover', function() {
-		$(this).animate({"background-positionY":52},1000);
-	});	
-	$social.on('mouseout', function() {
-		$(this).animate({"background-positionY" : 0},1000);
-	});		
-})*/
+$(function(){
+/*	var maxScrollTop=200;
+	$(window).scroll(function(event){
+		$(".header").slideUp(1000);
+		$(".nav").css({
+			position: 'fixed',
+			top: '0',
+			opacity: "1"
+		});
+	});*/
+	$(window).one("scroll",function(){
+		$(".header").slideUp(1000);
+		$(".nav").css({
+			position: 'fixed',
+			top: '0',
+			opacity: "1"
+		});
+		$(window).scrollTop(0);
+	})
+})
